eeaf2c32fc36a4ce203e6beaefadd85a06ece0ff
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-03-08 15:29:23 -0600

    Adding draggable library to task

--- task-management-app/src/components/task.tsx
+++ task-management-app/src/components/task.tsx
@@ -1,11 +1,21 @@
 import { TaskProps } from '@/types';
 import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 
-const Task = ({ task }: TaskProps) => {
+const Task = ({ task, index }: TaskProps) => {
   return (
-    <div className="mb-2 rounded-sm border border-gray-300 p-2">
-      {task.content}
-    </div>
+    <Draggable draggableId={task.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          className="mb-2 rounded-sm border border-gray-300 p-2"
+        >
+          {task.content}
+        </div>
+      )}
+    </Draggable>
   );
 };
 
