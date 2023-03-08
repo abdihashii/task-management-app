9cbd403b5c4c2fad52d57c18cdaf59deccd2da6d
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-03-08 15:28:53 -0600

    adding dragDropcontext

--- task-management-app/src/pages/index.tsx
+++ task-management-app/src/pages/index.tsx
@@ -3,11 +3,17 @@ import { Inter } from 'next/font/google';
 import { initialData } from '../data/initial-data';
 import { useState } from 'react';
 import Column from '@/components/column';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const [data, setData] = useState(initialData);
+
+  // Synchronously update the state to reflect the drag and drop result
+  const onDragEnd = (result) => {
+    // TODO: reorder our column
+  };
 
   return (
     <>
@@ -15,12 +21,21 @@ export default function Home() {
         <title>Task Management App</title>
       </Head>
       <main className={`${inter.className}`}>
-        {data.columnOrder.map((columnId) => {
-          const column = data.columns[columnId];
-          const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
+        <DragDropContext
+          // called when the drag starts
+          // onDragStart
+          // called when something changes during the drag, such as moving over a new droppable
+          // onDragUpdate
+          // called at the end of the drag, this is the only required callback
+          onDragEnd={onDragEnd}
+        >
+          {data.columnOrder.map((columnId) => {
+            const column = data.columns[columnId];
+            const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
 
-          return <Column key={column.id} {...{ column, tasks }} />;
-        })}
+            return <Column key={column.id} {...{ column, tasks }} />;
+          })}
+        </DragDropContext>
       </main>
     </>
   );
