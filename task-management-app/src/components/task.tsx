5948f6151aeac36f65e9ff0124bb0bea23f71877
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-03-08 16:06:02 -0600

    adding draggable conditional styling using snapshot.isDragging

--- task-management-app/src/components/task.tsx
+++ task-management-app/src/components/task.tsx
@@ -5,12 +5,15 @@ import { Draggable } from 'react-beautiful-dnd';
 const Task = ({ task, index }: TaskProps) => {
   return (
     <Draggable draggableId={task.id} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <div
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
-          className="mb-2 rounded-sm border border-gray-300 bg-white p-2"
+          className={`mb-2 rounded-sm border border-gray-300 p-2 ${
+            snapshot.isDragging ? 'bg-orange-400' : 'bg-white'
+          }`}
+          data-is-dragging={snapshot.isDragging} // using data-is-dragging because isDragging is not a supported attribute for HTMLElements
         >
           {task.content}
         </div>
@@ -20,3 +23,26 @@ const Task = ({ task, index }: TaskProps) => {
 };
 
 export default Task;
+
+/****
+snapshot object: 
+  contains a number of properties that can be used to style 
+  the component during a drag and drop operation
+
+// Draggable
+const draggableSnapshot = {
+  isDragging: boolean, // will be true if the item is being dragged
+  draggingOver: string | null, // will be the id of the droppable that the 
+                                  item is being dragged over, or null if it 
+                                  is not being dragged over any droppable
+};
+
+// Droppable
+const droppableSnapshot = {
+  isDraggingOver: boolean, // will be true when a draggable is dragging over 
+                              this droppable
+  draggingOverWith: string | null, // will be the id of the draggable that is
+                                      being dragged over this droppable, or
+                                      null if it is not being dragged over
+};
+*/
