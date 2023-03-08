28e3ef43de652128215617df8bf535d65466cea9
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-03-08 14:15:33 -0600

    init next and tailwind add

---
+++ task-management-app/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Task Management App</title>
+      </Head>
+      <main className={`${inter.className}`}></main>
+    </>
+  );
+}
